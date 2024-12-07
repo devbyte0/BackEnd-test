bf6dc243fa7183e2e7e01ec9ecd6a38725a1903a
“[rezatahseenfiverr]” <[rezatahseenfiverr2gmail.com]>
2024-12-08 05:32:11 +0600

    M

--- index.js
+++ index.js
@@ -49,11 +49,11 @@ app.use(express.json());
 // Serve static files from the dist folder (for Vite React app)
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname, "/home/ubuntu/Frontend-test/dist"))); // Serve static files from dist folder
+app.use(express.static(path.join(__dirname, "../Frontend-test/dist"))); // Serve static files from dist folder
 
 // Redirect all routes to index.html (for frontend routes)
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, "/home/ubuntu/Frontend-test/dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../Frontend-test/dist", "index.html"));
 });
 
 // Define API routes
